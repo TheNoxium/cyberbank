926db298b909c03e13db9902bd30d0f3a0b00eae
TheNoxium <113540766+TheNoxium@users.noreply.github.com>
2023-03-25 06:32:27 +0500

    вычитание

--- content.php
+++ content.php
@@ -1,29 +1,46 @@
-
 <pre>
     <?php
 
-if (isset($_POST['edit1'])) {
+    if (isset($_POST['edit1'])) {
 
 
-$Login = $_POST['logintransfer'];
+        $Logintransfer = $_POST['logintransfer'];
 
-mysqli_real_escape_string($db, $_POST['logintransfer']);
-    
-$logtrans = mysqli_query($db,"SELECT * FROM `Accounts` WHERE `login`='$Login'");
+        mysqli_real_escape_string($db, $_POST['logintransfer']);
 
-$logtrans2 = mysqli_fetch_assoc($logtrans);
+        $logtrans = mysqli_query($db, "SELECT * FROM `Accounts` WHERE `login`='$Logintransfer'");
+
+        $logtrans2 = mysqli_fetch_assoc($logtrans);
+
+        $balanstransfer = $_POST['balanstransfer'];
+
+        $balans2 = htmlspecialchars($logtrans2['Balans']);
+        $balans = htmlspecialchars($user['Balans']);
+
+        $balansfinal = $balans - $balanstransfer;
+        echo $balansfinal;
+
+
+        $balansfinaltransfer = mysqli_real_escape_string($db, $balansfinal);
+       
+
+        if (mysqli_query($db, "UPDATE Accounts SET Balans='$balansfinaltransfer' WHERE `login`='$Logintransfer'")) {
+            
+            echo 'Операция выполнена успешно!';
+        } else {
+            header('Refresh: 10');
+            echo 'Ошибка. Изменения не были сохранены. Страница обновится через 10 секунд.';
+        }
 
 
 
-$balans2 = htmlspecialchars($logtrans2['Balans']);
-$balans = htmlspecialchars($user['Balans']);
 
-echo $balans - $balans2;
-
-}
-?>
+    }
+    ?>
 </pre>
 
+Ваш баланс:
+<?php echo htmlspecialchars($user['Balans']); ?>
 
 <div id="edit">
     <form method="post">
@@ -33,8 +50,11 @@ echo $balans - $balans2;
         </h3>
 
         <label for="logintransfer">Логин куда вы хотите послать бабки</label>
-        <input name="logintransfer" type="text" id="logintransfer" placeholder="Введите логин"
-            value="<?php echo htmlspecialchars($user['login']); ?>" required>
+        <input name="logintransfer" type="text" id="logintransfer" placeholder="Введите логин" required>
+        <br><br>
+
+        <label for="balanstransfer">Сколько бабок вы хотите перевести </label>
+        <input name="balanstransfer" type="text" id="balanstransfer" placeholder="Введите сумму" required>
         <br><br>
 
         <button type="submit" name="edit1" class="">Сохранить</button>
